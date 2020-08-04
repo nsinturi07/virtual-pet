7e702b76dd98eed58e6b1f13ec403ad0d79a24e8
nsinturi07 <65034759+nsinturi07@users.noreply.github.com>
2020-08-03 22:08:56 -0500

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,22 @@
+class Food{
+    constructor(){
+
+    }
+    display(){
+        var x=80,y=100
+
+        imageMode(CENTER);
+        image(this.image,720,220,70,70);
+
+        if(this.foodStock!=0){
+            for(var i=0;i<this.foodstock;i++){
+                if(i%10==0){
+                    x=80;
+                    y=y+50;
+                }
+                image(this.image,x,y50,50);
+                x=x+30;
+            }
+        }
+    }
+}
